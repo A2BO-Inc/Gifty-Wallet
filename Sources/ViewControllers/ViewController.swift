c59736a861d35c795c0b7c8812c9a8ec017cebb0
yeohun.jeon <yeohun.jeon@linecorp.com>
2022-07-28 02:30:06 +0900

    [Chore] Add Tuist setup

---
+++ Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Gifty-Wallet
+//
+//  Created by yeohun-jeon on 2022/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
